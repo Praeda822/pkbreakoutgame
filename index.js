29dafb0a8c4b3061839b1e55730b879a36691fa6
Patrick Kelly <patdkelly5@gmail.com>
2024-07-16 13:47:28 +1000

    Added blocks to project using constructor

--- index.js
+++ index.js
@@ -13,11 +13,36 @@ class Block {
   }
 }
 
-const mkBlock = function () {
-  const block = document.createElement('div');
-  block.classList.add('block');
-  block.style.left = '100px';
-  block.style.bottom = '50px';
-  grid.appendChild(block);
+const blocks = [
+  // x axis top row
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  // y axis upper-middle row
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  // y axis lower-middle row
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210),
+];
+
+console.log(blocks[0]);
+
+const mkBlocks = function () {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement('div');
+    block.classList.add('block');
+    block.style.left = blocks[i].bottomLeft[0] + 'px';
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+    grid.appendChild(block);
+  }
 };
-mkBlock();
+mkBlocks();
