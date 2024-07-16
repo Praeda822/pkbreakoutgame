fa275b193fbc9206c3783a56414b2b455bd48336
Patrick Kelly <patdkelly5@gmail.com>
2024-07-16 13:32:04 +1000

    block maker class to create blocks

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-"use strict";
+'use strict';
+
+const grid = document.querySelector('.grid');
+const blockWidth = 100;
+const blockHeight = 20;
+
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
+
+const mkBlock = function () {
+  const block = document.createElement('div');
+  block.classList.add('block');
+  block.style.left = '100px';
+  block.style.bottom = '50px';
+  grid.appendChild(block);
+};
+mkBlock();
